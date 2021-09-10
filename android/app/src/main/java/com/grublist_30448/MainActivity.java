8d13fafb757fe7e1ada143f7ad191bc8bb2ad8ea
Crowdbotics <team@crowdbotics.com>
2021-09-10 19:35:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/grublist_30448/MainActivity.java
@@ -0,0 +1,15 @@
+package com.grublist_30448;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "grublist_30448";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "grublist_30448Identifier",
+  url: "https://grublist_30448Identifier.botics.co",
+  api: "https://grublist_30448Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
